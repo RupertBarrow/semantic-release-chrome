aa0a5672ab3d3c9977cefc9e3fe6505de5a9cad4
RupertBarrow <rupert.barrow@lilo.org>
2024-02-14 17:56:15 +0100

    feat: Converted channel to a digit

--- src/prepare.spec.ts
+++ src/prepare.spec.ts
@@ -3,19 +3,30 @@ import { parsePrereleaseVersion } from './prepare'
 import SemanticReleaseError from '@semantic-release/error'
 
 describe('parsePrereleaseVersion', () => {
-    test('success case - typical semantic-release prerelease version string', () => {
-        expect(parsePrereleaseVersion('1.0.0-develop.1')).toEqual('1.0.0')
-    })
+  test('success case - typical semantic-release prerelease version string (develop)', () => {
+    expect(parsePrereleaseVersion('1.0.0-develop.1')).toEqual('1.0.0.4000001')
+  })
 
-    test('success case - normal semantic-release version string', () => {
-        expect(parsePrereleaseVersion('1.0.0')).toEqual('1.0.0')
-    })
+  test('success case - typical semantic-release prerelease version string (alpha)', () => {
+    expect(parsePrereleaseVersion('1.3.0-alpha.2')).toEqual('1.3.0.2000002')
+  })
+  test('success case - semantic-release prerelease version string with unknown channel', () => {
+    expect(parsePrereleaseVersion('1.3.0-unknown.2')).toEqual('1.3.0.0000002')
+  })
 
-    test('failure case - no semantic-release version in string', () => {
-        expect(() => { parsePrereleaseVersion('develop.1') }).toThrow(SemanticReleaseError)
-    })
+  test('success case - normal semantic-release version string', () => {
+    expect(parsePrereleaseVersion('1.0.0')).toEqual('1.0.0')
+  })
 
-    test('failure case - empty string', () => {
-        expect(() => { parsePrereleaseVersion('') }).toThrow(SemanticReleaseError)
-    })
+  test('failure case - no semantic-release version in string', () => {
+    expect(() => {
+      parsePrereleaseVersion('develop.1')
+    }).toThrow(SemanticReleaseError)
+  })
+
+  test('failure case - empty string', () => {
+    expect(() => {
+      parsePrereleaseVersion('')
+    }).toThrow(SemanticReleaseError)
+  })
 })
--- src/prepare.ts
+++ src/prepare.ts
@@ -50,8 +50,7 @@ const zipFolder = (
 ) => {
   const zipPath = resolve(asset)
   const output = createWriteStream(zipPath)
-  const archive = archiver('zip', {
-  })
+  const archive = archiver('zip', {})
 
   archive.pipe(output)
 
@@ -61,13 +60,25 @@ const zipFolder = (
   logger.log('Wrote zipped file to %s', zipPath)
 }
 
+const MAP_CHANNEL_TO_NUMBER = {
+  nightly: '1',
+  alpha: '2',
+  beta: '3',
+  develop: '4',
+  master: '6',
+  latest: '7',
+  next: '8',
+  'next-major': '9',
+}
 /**
  * Attempts to parse a semantic version number from a pre-release version string.
  *
  * Context:
- * The semantic-release package will provide a version string such as '1.0.0-develop.1' when using the
- * pre-release functionality. This function will parse out the semantic version number '1.0.0' from this
+ * The semantic-release package will provide a version string such as '2.3.5-develop.1' when using the
+ * pre-release functionality. This function will parse out the semantic version number '2.3.5.4000001' from this
  * string, so that the version will adhere to the chrome web store's version format requirement.
+ *
+ * The build version is built from the channel name (mapped to a digit) and the build number, padded with 0s.
  *
  * @param prereleaseVersion pre-release version string from which to parse the semantic version number
  * @returns semantic version number parsed from prereleaseVersion input. throws error if unable to parse
@@ -79,7 +90,18 @@ export const parsePrereleaseVersion = (prereleaseVersion: string) => {
       'Could not parse semantic version number from pre-release version',
     )
   }
-  return versionMatch[0]
+  const majorMinorPatchVersion = versionMatch?.[0] // eg 1.3.2
+
+  const prereleaseMatch = prereleaseVersion?.match(/-(\w+)\.(\d+)/)
+  if (!prereleaseMatch) {
+    throw new SemanticReleaseError('Could not parse pre-release version')
+  }
+
+  const [, channelName, buildNumber] = prereleaseMatch // eg develop, 1
+  const channelNumber = MAP_CHANNEL_TO_NUMBER[channelName] ?? '0'
+  const buildVersion = `${channelNumber}${buildNumber.padStart(6, '0')}` // eg 4000001
+
+  return `${majorMinorPatchVersion}.${buildVersion}`
 }
 
 const prepare = (
